8b23c626e9d592ca6df3920dcd3ae2d7ddc3b1a5
FrankLi123 <75002277+FrankLi123@users.noreply.github.com>
2022-02-11 00:44:35 -0800

    2/9 back-end interface + connection to database

--- src/main/java/com/hope/dao/ProductRepository.java
+++ src/main/java/com/hope/dao/ProductRepository.java
@@ -2,6 +2,9 @@ package com.hope.dao;
 
 import com.hope.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
