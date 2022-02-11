999a656d977b7c865b52772f3d8fa241225c3acf
FrankLi123 <zoli@ucsd.edu>
2022-02-11 00:37:19 -0800

    Angular components set up

--- front-end/angular-ecommerce/src/app/app.module.ts
+++ front-end/angular-ecommerce/src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import {HttpClientModule} from '@angular/common/http'
+import { ProductService } from './services/product.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
